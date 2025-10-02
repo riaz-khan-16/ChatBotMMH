84b6b0c25c3babd74ce3a423e1e8dcb9abcf1742
riaz-khan-16 <goriazmte@gmail.com>
2025-10-02 12:57:25 +0600

    frontend and backend is connected

--- Chatbot/urls.py
+++ Chatbot/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-
     path('', home, name='homepage'),
-    
+    path('get-response/', ai_response, name='ai_response' ),
 ]
+
--- Chatbot/views.py
+++ Chatbot/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-from django.http import request
+from django.http import request,JsonResponse
 
 # Create your views here.
 
 def home(request):
     return render(request,"index.html")
+
+def ai_response(request):
+    if request.method == "POST":
+        # Get the user message from request
+        user_message = request.POST.get("message", "")
+        # Simple rule-based response
+        if "hello" in user_message.lower():
+            reply = "Hi there! How can I help you?"
+        elif "bye" in user_message.lower():
+            reply = "Goodbye! Have a nice day!"
+        else:
+            reply = "Sorry, I don't understand that."
+        # Return JSON response
+        return JsonResponse({"reply": reply})
+    return JsonResponse({"reply": "Invalid request."})
