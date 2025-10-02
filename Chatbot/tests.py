5c2ea46c45061a846f1815cebbf4cc0b85b02c96
riaz-khan-16 <goriazmte@gmail.com>
2025-10-02 13:21:25 +0600

    automated testing files are added

--- Chatbot/tests.py
+++ Chatbot/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+
+class SimpleTest(TestCase):
+    def test1(self):
+        # This test will pass because 1 + 1 == 2
+        self.assertEqual(1 + 1, 2)
